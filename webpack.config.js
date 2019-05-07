8d2063cd0cfb2283b4c873e7b5c2bd2aa0a9bf54
Hooks Zhang <hookszhang@gmail.com>
2019-05-07 14:01:09 +0800

    feat: output management

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,6 +8,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Cruise'
+    })
+  ],
   module: {
     rules: [
       {
