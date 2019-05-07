d424c3206740ca7c1b4930bc27b0fd771812f950
Hooks Zhang <hookszhang@gmail.com>
2019-05-07 14:51:24 +0800

    feat: base layout

--- src/index.js
+++ src/index.js
@@ -2,15 +2,3 @@ import _ from 'lodash';
 import './style.css';
 import './fonts.css';
 
-function component() {
-  var element = document.createElement('div');
-
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  console.error('I get called from index.js!');
-  return element;
-}
-
-document.body.appendChild(component());
