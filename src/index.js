9cbc036976dd09dede13e59b6af6ac9836f99f0a
Hooks Zhang <hookszhang@gmail.com>
2019-05-08 12:30:08 +0800

    feat: popper

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './styles/main.scss'
 import './router'
+
+import Popper from './components/popper/popper.html'
+
+console.log(Popper)
+
+function bindEventListener () {
+  const btnPlus = document.querySelectorAll('.btn.plus')
+  btnPlus.forEach(btn => {
+    btn.addEventListener('click', () => {
+      console.log('on click', this)
+    })
+  })
+}
+
+bindEventListener()
+
+window.removeResource = function (event) {
+  console.log(event)
+}
